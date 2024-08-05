ca303721946a9cdf2bf953e36ba6e66ace5529d3
Artem Repko <a.repko33@gmail.com>
2024-08-05 12:11:16 +0300

    first commit

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+
+function checkForSpam(message) {
+  let messageLowerCase = message.toLowerCase();
+  if (messageLowerCase.includes('spam') || messageLowerCase.includes('sale')){
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
